bea80656b9718f081de65581d381e66468f0b4fa
RomainMAURICE <78416232+RomainMAURICE@users.noreply.github.com>
2023-04-04 15:28:17 +0200

    Add files via upload

--- TP10/main.c
+++ TP10/main.c
@@ -27,18 +27,56 @@ typedef struct cell{
 }Cell, *List;
 
 
+Cell* allocate_cell(char* first, char* last, int age){
+
+}
+
+int age_order(Cell* p1, Cell* p2){
+
+}
+
+int name_order(Cell* p1, Cell* p2){
+
+}
+
+void ordered_insertion(List* l, Cell* new, int order_func(Cell*, Cell*)){
+
+}
+
+void print_list(List l){
+    while(l!=NULL){
+        printf("%f ", l->first_name);
+        printf("%f ", l->last_name);
+        printf("%f\n", l->age);
+        l=l->next;
+    }
+}
+
+void free_list(List l){
+
+}
+
+
 int main(int argc, char const *argv[])
 {
+    List l;
+    Cell* c;
+    Cell c2;
+    l->age;
+    c->age;
+    c2.age;
+    
 
 
-    FILE *f = fopen(argv[1], "r");
+
+    /*FILE *f = fopen(argv[1], "r");
 
     if (f == NULL ){ fprintf(f,"can't open file %s", argv[1]); exit(1);}
 
     int i;
     //for(i=0;!=EOF;i++);
 
-    printf("%d", i);
+    printf("%d", i);*/
 
 
     return 0;
