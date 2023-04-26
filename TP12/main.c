3c00bdb3fb8f4a7f01d0aee89fa621e697db43c6
RomainMAURICE <78416232+RomainMAURICE@users.noreply.github.com>
2023-04-26 23:47:12 +0200

    Add files via upload

--- TP12/main.c
+++ TP12/main.c
@@ -7,42 +7,72 @@ typedef struct node {
     struct node *next;
 }Node, *List;
 
-void initListe(char *word, Node node){
+Node *insertFirst(Node *next, char *word){
     Node *node = malloc(sizeof(Node));
-    node.
+    node->word = malloc(strlen(word)+1);
+    strcpy(node->word, word);
+    node->next = next;
+    return node;
 }
+
+int count(Node *node){
+    int total=0;
+    while(node){
+        total++;
+        node = node->next;
+    }
+    return total;
+}
+
+int search(Node *node, char *word){
+    while(node){
+        if(strcmp(node->word, word)==0){
+            return 1;
+        }
+        node = node->next;
+    }
+    return 0;
+}
+
+void freeList(Node *node){
+    while(node){
+        Node *tmp = node;
+        node = node->next;
+        free(tmp->word);
+        free(tmp);
+    }
+}
+
 
 int main(int argc, char const *argv[])
 {
     FILE *f = fopen("Germinal.txt","r");
     if (f == NULL ){ fprintf(f,"can't open file %s", "Germinal.txt"); exit(1);}
     
-    //Node *node = (Node*) malloc(sizeof(node));
-
     char mot[100];
-    int nbrMot=0;
-    List list;
+    int nbrTotalMots=0;
+    Node *node = NULL;
 
     while (fscanf(f, "%s", mot)==1){
-        List *list = (List*) malloc(sizeof(list));
-        Node *node = (Node*) malloc(sizeof(mot));
-
-
-        node->word = mot;
-        if(list vide){
-            node->next = NULL; 
-        }else{
-
+        if(!search(node, mot)){
+            node = insertFirst(node, mot);
         }
-
-
-
-
-        printf("%s ", node->word);
-        nbrMot++;
+        nbrTotalMots++;
     }
-    printf("%d", nbrMot);
 
+    int nbrMot=count(node);
+    printf("Nombre de mots uniques : %d \n", nbrMot);
+    printf("Nombre total de mots : %d \n", nbrTotalMots);
 
+    freeList(node);
+
+    /*time
+    Nombre de mots uniques : 25353
+    Nombre total de mots : 165960
+
+    real    0m12.766s
+    user    0m12.734s
+    sys     0m0.016s
+    */
     return 0;
 }
